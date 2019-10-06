88dca607160200041674e6b3a1f94fe57dd5e6ba
mak-454 <ahmed.khan@oneconvergence.com>
2019-10-06 12:50:32 -0700

    corrected the details

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dkube
-This repository is for client side SDK of Dkube platform. Applications can use this to interact with Dkube.
+Repository for dkube sdk and libraries which can be used by client side applications to interface with Dkube platform.
+
+Prerequisites
+-------------
+>=python3
+
+sudo pip install -r requirements.txt or
+sudo pip3 install -r requirements.txt
+
+How to install using pip
+------------------------
+sudo pip install git+https://github.com/oneconvergence/dkube.git or
+sudo pip3 install git+https://github.com/oneconvergence/dkube.git
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='dkube',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='ahmed.khan',
-      author_email='ahmed.khan@oneconvergence.com',
-      url='git+https://github.com/mak-454/dkube.git',
+      version='0.1',
+      description='Dkube SDK',
+      author='oneconvergence',
+      author_email='www.oneconvergence.com',
+      url='git+https://github.com/oneconvergence/dkube.git',
       packages = ['dkube', 'dkube.sdk', 'dkube.sdk.env', 'dkube.sdk.rest', 'dkube.sdk.schema']
      )
