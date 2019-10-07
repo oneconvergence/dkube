47625c053dfcd20d837fccf48af93276f6daaf87
mak-454 <ahmed.khan@oneconvergence.com>
2019-10-07 02:05:05 -0700

    added requires section to install deps

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='dkube',
       version='0.1',
       description='Dkube SDK',
       author='oneconvergence',
       author_email='www.oneconvergence.com',
       url='git+https://github.com/oneconvergence/dkube.git',
-      packages = ['dkube', 'dkube.sdk', 'dkube.sdk.env', 'dkube.sdk.rest', 'dkube.sdk.schema']
+      packages = ['dkube', 'dkube.sdk', 'dkube.sdk.env', 'dkube.sdk.rest', 'dkube.sdk.schema'],
+      install_requires=required
      )
