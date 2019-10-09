accfafaedef740f21bb866a8c462a3aa31ee41c6
mak-454 <ahmed.khan@oneconvergence.com>
2019-10-09 06:10:00 -0700

    installing all packages recursively

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+#from distutils.core import setup
+
+from setuptools import setup, find_packages
+
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -11,6 +14,7 @@ setup(name='dkube',
       author='oneconvergence',
       author_email='www.oneconvergence.com',
       url='git+https://github.com/oneconvergence/dkube.git',
-      packages = ['dkube', 'dkube.sdk', 'dkube.sdk.env', 'dkube.sdk.rest', 'dkube.sdk.schema', 'dkube.pipelines'],
+      #packages = ['dkube', 'dkube.sdk', 'dkube.sdk.*env', 'dkube.sdk.rest', 'dkube.sdk.schema', 'dkube.pipelines'],
+      packages = find_packages(),
       install_requires=required
      )
