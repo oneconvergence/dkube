c56ca8e7ce1b3b3d01af5294745914391edae630
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-10-23 14:36:34 +0530

    read and write using config

--- dkube/sdk/rsrcs/featureStore.py
+++ dkube/sdk/rsrcs/featureStore.py
@@ -2,28 +2,58 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from numpy import random
-import os
+import os, json
 
 class FeatureStore(object):
-    FEATURE_TYPE = ['dataframe', 'numpyarray']
+    FEATURE_TYPE = ['dataframe']
+    CONFIG_FILE = "/opt/dkube/fs/config.json"
 
-    def read(dtype = 'dataframe', featureset, path):
+    def list_fs(self):
+        fs = []
+        try:
+            with open(CONFIG_FILE) as json_file:
+                fsconfig = json.load(json_file)
+            featuresets = fsconfig["inputs"]["featuresets"]
+            for each_feature in featuresets:
+                fs.append([each_feature["name"], each_feature["location"]])
+            return fs
+        except Exception as e:
+            return e
+
+    def read(self, featureset, path = None):
         df_empty = pd.DataFrame({'A' : []})
-        if dtype not in FeatureStore:
-            err = 'data type not supported, supported types dataframe, numpyarray'
-            return df_empty, err
+        if path == None:
+            with open(CONFIG_FILE) as json_file:
+                fsconfig = json.load(json_file)
+            featuresets = fsconfig["inputs"]["featuresets"]
+            for each_feature in featuresets:
+                if each_feature["name"] == featureset:
+                    path = each_feature["location"]
+        if path == None:
+            fs = list_fs()
+            msg = "feature " + str(featureset) + "doesn't exist, avaiable featuresets are "
+            fs_list = ", ".join(fs)
+            return msg + fs_list
         try:
             table = pq.read_table(os.path.join(path,'example.parquet'))
             feature_df = table.to_pandas()
-            if dtype = 'dataframe':
-                return feature_df, 'none'
-            elif dtype = 'numpyarray':
-                return featureset.to_numpy(), 'none'
+            return feature_df, 'none'
         except Exception as e:
             return df_empty, e
 
-    def write(dataframe, featureset, path):
+    def write(self, dataframe, featureset, path = None):
+        if path == None:
+            with open(CONFIG_FILE) as json_file:
+                fsconfig = json.load(json_file)
+            featuresets = fsconfig["outputs"]["featuresets"]
+            for each_feature in featuresets:
+                if each_feature["name"] == featureset:
+                    path = each_feature["location"]
+        if path == None:
+            fs = list_fs()
+            msg = "feature " + str(featureset) + "doesn't exist, avaiable featuresets are "
+            fs_list = ", ".join(fs)
+            return msg + fs_list
         try:
             table = pa.Table.from_pandas(dataframe)
             pq.write_table(table, os.path.join(path,'example.parquet'))
