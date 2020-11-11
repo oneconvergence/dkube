f78d637d5f5ba68378f7b30bf202c41ed92b4b37
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-11-11 21:03:02 +0530

    fixes

--- dkube/sdk/internal/dkube_api/api/__init__.py
+++ dkube/sdk/internal/dkube_api/api/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
-# flake8: noqa
-
 # import apis into api package
-from swagger_client.api.dfab_api import DfabApi
-from swagger_client.api.dkube_api import DkubeApi
-from swagger_client.api.dkube_operator_exclusive_api import DkubeOperatorExclusiveApi
+from dkube.sdk.internal.dkube_api.api.dfab_api import DfabApi
+from dkube.sdk.internal.dkube_api.api.dkube_api import DkubeApi
+from dkube.sdk.internal.dkube_api.api.dkube_operator_exclusive_api import \
+    DkubeOperatorExclusiveApi
+
+# flake8: noqa
