2a276c3738e9c057142281c669c65bca6f75e833
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-11-11 21:40:48 +0530

    fixes

--- dkube/sdk/api.py
+++ dkube/sdk/api.py
@@ -16,7 +16,7 @@ import pandas as pd
 import pyarrow as pa
 from dkube.sdk.internal.api_base import *
 from dkube.sdk.rsrcs import *
-from dkube.sdk.rsrcs.featureset import DkubeFeatureSet
+from dkube.sdk.rsrcs.featureSet import DkubeFeatureSet
 
 
 class DkubeApi(ApiBase):
