000079933db09dcd29ebac4cdf00959befb74bd5
songole <songole@oneconvergence.com>
2020-11-16 22:08:38 -0800

    training updates

--- dkube/sdk/rsrcs/training.py
+++ dkube/sdk/rsrcs/training.py
@@ -1,27 +1,45 @@
 from __future__ import print_function
-import time
+
 import sys
+import time
+from pprint import pprint
 
 from dkube.sdk.internal import dkube_api
-from dkube.sdk.internal.dkube_api.rest import ApiException
-from dkube.sdk.internal.dkube_api.models.job_model import JobModel
-from dkube.sdk.internal.dkube_api.models.job_model_parameters import JobModelParameters
-from dkube.sdk.internal.dkube_api.models.ds_job_model import DSJobModel
-from dkube.sdk.internal.dkube_api.models.ds_job_model_executor import DSJobModelExecutor
-from dkube.sdk.internal.dkube_api.models.dkube_container_model import DkubeContainerModel
-from dkube.sdk.internal.dkube_api.models.dkube_container_model_framework import DkubeContainerModelFramework
-from dkube.sdk.internal.dkube_api.models.dkube_container_model_framework_details import DkubeContainerModelFrameworkDetails
-from dkube.sdk.internal.dkube_api.models.job_datum_model import JobDatumModel
-from dkube.sdk.internal.dkube_api.models.job_datum_model_workspace import JobDatumModelWorkspace
-from dkube.sdk.internal.dkube_api.models.job_input_datum_model import JobInputDatumModel
-from dkube.sdk.internal.dkube_api.models.job_model_parameters_run import JobModelParametersRun
-from dkube.sdk.internal.dkube_api.models.ds_job_model_hyperparams import DSJobModelHyperparams
+from dkube.sdk.internal.dkube_api.models.config_file_model import \
+    ConfigFileModel
+from dkube.sdk.internal.dkube_api.models.custom_container_model import \
+    CustomContainerModel
+from dkube.sdk.internal.dkube_api.models.custom_container_model_image import \
+    CustomContainerModelImage
 from dkube.sdk.internal.dkube_api.models.custom_kv_model import CustomKVModel
-from dkube.sdk.internal.dkube_api.models.config_file_model import ConfigFileModel
-from dkube.sdk.internal.dkube_api.models.custom_container_model import CustomContainerModel
-from dkube.sdk.internal.dkube_api.models.custom_container_model_image import CustomContainerModelImage
-from dkube.sdk.internal.dkube_api.models.ds_job_model_hptuning import DSJobModelHptuning
-from pprint import pprint
+from dkube.sdk.internal.dkube_api.models.dkube_container_model import \
+    DkubeContainerModel
+from dkube.sdk.internal.dkube_api.models.dkube_container_model_framework import \
+    DkubeContainerModelFramework
+from dkube.sdk.internal.dkube_api.models.dkube_container_model_framework_details import \
+    DkubeContainerModelFrameworkDetails
+from dkube.sdk.internal.dkube_api.models.ds_job_model import DSJobModel
+from dkube.sdk.internal.dkube_api.models.ds_job_model_executor import \
+    DSJobModelExecutor
+from dkube.sdk.internal.dkube_api.models.ds_job_model_hptuning import \
+    DSJobModelHptuning
+from dkube.sdk.internal.dkube_api.models.ds_job_model_hyperparams import \
+    DSJobModelHyperparams
+from dkube.sdk.internal.dkube_api.models.job_datum_model import JobDatumModel
+from dkube.sdk.internal.dkube_api.models.job_datum_model_workspace import \
+    JobDatumModelWorkspace
+from dkube.sdk.internal.dkube_api.models.job_featureset_model import \
+    JobFeaturesetModel
+from dkube.sdk.internal.dkube_api.models.job_input_datum_model import \
+    JobInputDatumModel
+from dkube.sdk.internal.dkube_api.models.job_input_featureset_model import \
+    JobInputFeaturesetModel
+from dkube.sdk.internal.dkube_api.models.job_model import JobModel
+from dkube.sdk.internal.dkube_api.models.job_model_parameters import \
+    JobModelParameters
+from dkube.sdk.internal.dkube_api.models.job_model_parameters_run import \
+    JobModelParametersRun
+from dkube.sdk.internal.dkube_api.rest import ApiException
 
 from .util import *
 
@@ -56,14 +74,18 @@ class DkubeTraining(object):
         self.input_datasets = []
         self.input_datums = JobDatumModel(
             workspace=self.input_project, datasets=self.input_datasets, outputs=self.output_models)
+        self.input_featuresets = []
+        self.output_featuresets = []
+        self.featuresets = JobFeaturesetModel(inputs=self.input_featuresets, outputs=self.output_featuresets)
         self.customkv = CustomKVModel()
         self.configfile = ConfigFileModel()
         self.customenv = []
         self.hyperparameters = DSJobModelHyperparams(
             file=self.configfile, custom=self.customenv)
         self.hptuning = DSJobModelHptuning()
+        
         self.training_def = DSJobModel(executor=self.executor_def, datums=self.input_datums,
-                                       rdma=False, hyperparams=self.hyperparameters, hptuning=self.hptuning)
+                                       rdma=False, hyperparams=self.hyperparameters, hptuning=self.hptuning, featuresets=self.featuresets)
         self.run_def = JobModelParametersRun(template=None, group='default')
         self.job_parameters = JobModelParameters(
             _class='training', training=self.training_def, run=self.run_def)
@@ -160,3 +182,7 @@ class DkubeTraining(object):
         self.training_def.nworkers = nworkers
         if opt == "auto":
             self.training_def.gpus_override = True
+
+    def add_input_featureset(self, name, version=None, mountpath=None):
+        featureset_model = JobInputFeaturesetModel(name=name, version=version, mountpath=mountpath)
+        self.input_featuresets.append(featureset_model)
