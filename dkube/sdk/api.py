5a5b7c3aa4f8533ae44695dbd11cd49af3ae8e4e
songole <songole@oneconvergence.com>
2020-12-01 14:41:59 -0800

    fix the service name for file upload

--- dkube/sdk/api.py
+++ dkube/sdk/api.py
@@ -65,7 +65,7 @@ class DkubeApi(ApiBase, FilesBase):
             self.url = os.getenv(
                 "DKUBE_ACCESS_URL", "http://dkube-controller-master.dkube.svc.cluster.local:5000")
             self.files_url = os.getenv(
-                "DKUBE_ACCESS_URL", "http://dkube-controller-worker.dkube.svc.cluster.local:5000")
+                "DKUBE_ACCESS_URL", "http://dkube-downloader.dkube.svc.cluster.local:9401")
         else:
             self.files_url = self.url
 
