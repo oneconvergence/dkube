e412e75edf2330f25bd98613e19295b8a7291cf9
riteshkarvaloc <ritesh.k@oneconvergence.com>
2020-12-03 18:27:56 +0530

    example update

--- dkube/sdk/api.py
+++ dkube/sdk/api.py
@@ -303,7 +303,6 @@ class DkubeApi(ApiBase, FilesBase):
                 print(
                     "run {} - waiting for completion, current state {}".format(run.name, state))
                 time.sleep(10)
-                break
 
     def get_test_inference(self, user, name):
         """
