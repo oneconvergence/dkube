4a735934b99646e45d6864e4c6d29ef55b79a41d
Rajendra <rajendra.singh@oneconvergence.com>
2021-01-14 17:54:07 -0600

    changes to support versioning

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.6
 
 RUN apt install -y git
-COPY requirements.txt .
+COPY launcher/requirements.txt .
 RUN pip install -r requirements.txt
-RUN pip install git+https://github.com/oneconvergence/dkube.git@pipeline --upgrade
+ADD dist /dist
+RUN pip install /dist/*.whl
 
-ADD src/ /dkubepl
+ADD launcher/src/ /dkubepl
 RUN python3 /dkubepl/main.py
 
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-setup(name='dkube',
-      version='2.2',
-      description='Dkube SDK',
-      author='oneconvergence',
-      author_email='www.oneconvergence.com',
-      url='git+https://github.com/oneconvergence/dkube.git',
-      packages = find_packages(),
-      install_requires=required,
-      python_requires='>3.5'
-     )
+setup(
+    name="dkube",
+    version="2.2",
+    description="Dkube SDK",
+    author="oneconvergence",
+    author_email="dkube@oneconvergence.com",
+    url="https://github.com/oneconvergence/dkube.git",
+    packages=find_packages(),
+    install_requires=required,
+    python_requires=">3.5",
+)
