5197ff22fdbb230d760b6ddf5c87888a14fe8bc6
riteshkarvaloc <ritesh.k@oneconvergence.com>
2021-02-23 19:03:22 +0530

    framework env

--- R/loads.R
+++ R/loads.R
@@ -1,5 +1,6 @@
 dkube <- NULL
 .onLoad <- function(libname, pkgname) {
   # use superassignment to update global reference to scipy
+  Sys.setenv(FRAMEWORK = "R")
   dkube <<- reticulate::import("dkube", delay_load = TRUE)
 }
