7ef2115446bc02c53b6920c83bceb725b5dfcd25
riteshkarvaloc <ritesh.k@oneconvergence.com>
2021-03-23 18:53:13 +0530

    r-read-write

--- dkube/sdk/rsrcs/featureset.py
+++ dkube/sdk/rsrcs/featureset.py
@@ -348,7 +348,7 @@ class DKubeFeatureSetUtils:
                 path = None
 
             if path is None:
-                dkube_path = "/tmp/fsdata/"
+                dkube_path = "/home/tmp/fsdata/"
                 # os.getenv('DKUBE_USER_STORE')
                 if dkube_path is None:
                     return None
