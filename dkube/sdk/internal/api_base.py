0f14b174a13cf2fbc1d9982b3bba00c70e797c23
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-09-20 19:26:42 +0530

    Update api_base.py

--- dkube/sdk/internal/api_base.py
+++ dkube/sdk/internal/api_base.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import os
 import time
 from pprint import pprint
@@ -29,6 +27,7 @@ class ApiBase(object):
         configuration.api_key['Authorization'] = token
         configuration.verify_ssl = False
         self._api = dkube_api.DkubeApi(dkube_api.ApiClient(configuration))
+        self._mmapi = dkube_api.ModelmonitorApi(dkube_api.ApiClient(configuration))
         self.common_tags = list_of_strs(common_tags)
         self.wait_interval = 10
 
@@ -389,3 +388,67 @@ class ApiBase(object):
             dkube_api.ApiClient(configuration))
         response = api.get_all_deployments()
         return response.to_dict()['data']
+
+   #### MODEL MONITOR APIS ###
+
+    def create_model_monitor(self,modelmonitor):
+        response = self._mmapi.modelmonitor_add_one(data=modelmonitor.modelmonitor)
+        return response.to_dict()
+    
+    def get_modelmonitor(self,modelmonitor):
+        response = self._mmapi.modelmonitor_get(modelmonitor)
+        return response
+
+    def list_modelmonitor(self):
+        response = self._mmapi.modelmonitor_list()
+        return response.to_dict()['data']
+
+    def get_modelmonitor_configuration(self,modelmonitor_id):
+        response = self._mmapi.modelmonitor_get(modelmonitor_id)
+        return response.to_dict()['data']
+    
+    def get_modelmonitor_dataset(self,modelmonitor):
+        response = self._mmapi.modelmonitor_datasets_list(modelmonitor)
+        return response.to_dict()['data']
+
+    def get_modelmonitor_alerts(self,modelmonitor_id):
+        response = self._mmapi.modelmonitor_alerts_list(modelmonitor_id)
+        return response.to_dict()
+    
+    def get_modelmonitor_features(self,modelmonitor):
+        response = self._mmapi.modelmonitor_get_features(modelmonitor)
+        print(response)
+        return response.to_dict()['data']
+    
+    def get_modelmonitor_template(self):
+        response = self._mmapi.modelmonitor_get_metrics_template()
+        return response.to_dict()
+
+    def delete_modelmonitors(self,delete_list):
+        response = self._mmapi.modelmonitor_delete({'data': delete_list})
+        return response.to_dict()
+
+    def delete_modelmonitor_dataset(self,mm_id,delete_dataset_list):
+        response = self._mmapi.modelmonitor_delete_datasets(mm_id,{'data':delete_dataset_list})
+        return response.to_dict()
+    
+    def delete_modelmonitor_alert(self,mm_data,delete_alerts_list):
+        response = self._mmapi.modelmonitor_delete_alerts(mm_id,{data:delete_alerts_list})
+        return response.to_dict()
+   
+    def modelmonitor_addalert(self,modelmonitor,alert_data):
+        response = self._mmapi.modelmonitor_add_alerts(modelmonitor,alert_data)
+        return response.to_dict()
+    
+    def modelmonitor_adddataset(self,modelmonitor,dataset):
+        response = self._mmapi.modelmonitor_add_datasets(modelmonitor,dataset)
+        return response.to_dict()
+
+    def modelmonitor_archive(self,modelmonitor,archive):
+        response = self._mmapi.modelmonitor_archive(modelmonitor,archive)
+        return response.to_dict()
+
+    def modelmonitor_state(self,modelmonitor,state):
+        response = self._mmapi.modelmonitor_state(modelmonitor,state)
+        return response.to_dict()
+
