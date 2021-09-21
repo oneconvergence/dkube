a3192bb9df1f181620b97a7e5562b13b205d9941
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-09-21 20:03:24 +0530

    Update modelmonitor.py

--- dkube/sdk/rsrcs/modelmonitor.py
+++ dkube/sdk/rsrcs/modelmonitor.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import sys
+import sys,os
 import time
 from pprint import pprint
 from .util import *
@@ -15,6 +15,8 @@ from dkube.sdk.internal.dkube_api.models.modelmonitor_dataset_def import Modelmo
 from dkube.sdk.internal.dkube_api.models.modelmonitor_alert_cond_def import ModelmonitorAlertCondDef
 from dkube.sdk.internal.dkube_api.models.modelmonitor_alert_def import ModelmonitorAlertDef
 from dkube.sdk.internal.dkube_api.models.modelmonitor_def import ModelmonitorDef
+
+username =  os.getenv("USER")
 
 
 class DkubeModelMonitor(object):
@@ -71,9 +73,10 @@ class DkubeModelMonitor(object):
         
         return self
     
-    def add_dataset(self,name,data_class,version=None):
+    def add_dataset(self,name,data_class,version=None,data_format='csv'):
+        name = name + ":"+ username
         mm_dataset = ModelmonitorDatasetDef(id=None, _class=data_class, transformer_script=None, name=name, sql_query=None,
-                                               s3_subpath=None, version=version, data_format=None, groundtruth_col=None,
+                                               s3_subpath=None, version=version, data_format=data_format, groundtruth_col=None,
                                                predict_col=None, created_at=None, updated_at=None)
         
         self.modelmonitor.datasets.append(mm_dataset)
@@ -84,24 +87,23 @@ class DkubeModelMonitor(object):
         self.modelmonitor.alerts.append(mm_alert)
     
     def update_datasets(self,name,data_class,transformer_script=None,sql_query=None,groundtruth_col=None,predict_col=None):
-        self.datasets.name = name
-        self.datasets._class = data_class
-        self.datasets.transformer_script = transformer_script
-        self.datasets.sql_query = sql_query
-        self.datasets.groundtruth_col = groundtruth_col
-        self.datasets.predict_col = predict_col
+        self.modelmonitor.datasets.name = name
+        self.modelmonitor.datasets._class = data_class
+        self.modelmonitor.datasets.transformer_script = transformer_script
+        self.modelmonitor.datasets.sql_query = sql_query
+        self.modelmonitor.datasets.groundtruth_col = groundtruth_col
+        self.modelmonitor.datasets.predict_col = predict_col
         
     
     def update_alerts(self,name,alert_class,conditions=None):
-        self.alerts.name = name
-        self.alerts._class = alert_class
-        self.alerts.conditions = conditions
+        self.modelmonitor.alerts.name = name
+        self.modelmonitor.alerts._class = alert_class
+        self.modelmonitor.alerts.conditions = conditions
         
-
-            
+          
     def add_modelmonitor(self,name,model_name,model_type,model_category,model_framework,version,run_freq,drift_algo,emails):
         self.modelmonitor.name = name
-        self.modelmonitor.model= model_name
+        self.modelmonitor.model= model_name+":"+username
         self.modelmonitor.model_type = model_type
         self.modelmonitor.model_category = model_category
         self.modelmonitor.model_framework = model_framework
