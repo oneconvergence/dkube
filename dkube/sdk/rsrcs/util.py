c40867bff43e59048a9e42d9f1774c07b65edfb2
Tiji Thomas <tiji.thomas@oneconvergence.com>
2022-04-07 19:43:45 +0530

    Fix random generation in generate function

--- dkube/sdk/rsrcs/util.py
+++ dkube/sdk/rsrcs/util.py
@@ -1,7 +1,17 @@
 import string
 import random
+from datetime import datetime
 
-generate = lambda hint: "{}-{}".format(hint, ''.join([random.choice(string.digits) for n in range(4)]))
+def generate(prefix):
+    random.seed(datetime.now())
+    # truncate length of the prefix to 12 characters
+    prefix = prefix[:12]
+    #Total length = 16 characters
+    N = 16
+    #length of random characters to be generated
+    random_len = N - len(prefix) - 1
+    return "{}-{}".format(prefix, ''.join([random.choice(string.ascii_uppercase + string.ascii_lowercase +
+                             string.digits) for n in range(random_len)]))
 
 def list_of_strs(x):
     if x != None:
