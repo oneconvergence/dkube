ada3b8945a0751da771ceee4c349eb5648b69ffe
riteshkarvaloc <ritesh.k@oneconvergence.com>
2022-05-12 15:47:17 +0530

    setting default auth_type to none

--- dkube/sdk/api.py
+++ dkube/sdk/api.py
@@ -3021,6 +3021,8 @@ class DkubeApi(ApiBase, FilesBase):
         Outputs*
             a dictionary object with response status
         """
+        if data.auth_type is None:
+            raise ValueError("Auth type for cluster can't be None")
         clusters_info = []
         clusters_info.append(data)
         clusters_dict = {}
