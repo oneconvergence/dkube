305cd900f088f676f1475455e2b1ea039bbea3a9
AnsuFrancis <ansu.francis@oneconvergence.com>
2024-02-06 12:27:53 +0000

    formatting

--- dkube/sdk/mlflow_logger.py
+++ dkube/sdk/mlflow_logger.py
@@ -4,6 +4,7 @@ import mlflow
 import tempfile
 import os
 
+
 class DkubeMlflowLogger:
     def get_logger(self):
         logger = logging.getLogger()
@@ -11,20 +12,22 @@ class DkubeMlflowLogger:
             return logger
         else:
             logger.setLevel(logging.INFO)
-            log_formatter = logging.Formatter("%(asctime)s  %(name)s  |%(levelname)-5.5s|  %(message)s")
+            log_formatter = logging.Formatter(
+                "%(asctime)s  %(name)s  |%(levelname)-5.5s|  %(message)s")
             temp = tempfile.NamedTemporaryFile(suffix='.log', delete=False)
             file_handler = logging.FileHandler(temp.name)
             file_handler.setFormatter(log_formatter)
             logger.addHandler(file_handler)
-            
+
             console_handler = logging.StreamHandler(sys.stdout)
             console_handler.setFormatter(log_formatter)
             logger.addHandler(console_handler)
             return logger
-        
+
     def flush(self):
         logger = logging.getLogger()
-        log_paths = [handler.baseFilename for handler in logger.handlers if isinstance(handler, logging.FileHandler)]
+        log_paths = [handler.baseFilename for handler in logger.handlers if isinstance(
+            handler, logging.FileHandler)]
         log_file = next(iter(log_paths))
         if mlflow.active_run():
             print("Persisting logs in storage")
@@ -32,9 +35,3 @@ class DkubeMlflowLogger:
             os.remove(log_file)
         else:
             print("No active run found")
-        
-
-
-
-        
-    
