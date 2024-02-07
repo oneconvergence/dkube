bbbb8c7a6f9fad4ac119ab2b512835c547fb39cd
AnsuFrancis <ansu.francis@oneconvergence.com>
2024-02-07 12:17:29 +0000

    create fresh logger on every run

--- dkube/sdk/mlflow_logger.py
+++ dkube/sdk/mlflow_logger.py
@@ -6,35 +6,39 @@ import os
 
 
 class NotebookMlflowLogger:
+    def __init__(self):
+        self.logger = None
+        self.filename = ""
+
     def get_logger(self, level=logging.INFO):
         if os.getenv("DKUBE_JOB_CLASS") != "notebook":
             return logging.getLogger()
         logger = logging.getLogger()
-        if logger.handlers:
-            return logger
-        else:
-            logger.setLevel(level)
-            log_formatter = logging.Formatter(
-                "%(asctime)s  %(name)s  |%(levelname)-5.5s|  %(message)s")
-            temp = tempfile.NamedTemporaryFile(suffix='.log', delete=False)
-            file_handler = logging.FileHandler(temp.name)
-            file_handler.setFormatter(log_formatter)
-            logger.addHandler(file_handler)
+        logger.handlers.clear()
+        logger.setLevel(level)
+        log_formatter = logging.Formatter(
+            "%(asctime)s  %(name)s  |%(levelname)-5.5s|  %(message)s")
+        temp = tempfile.NamedTemporaryFile(suffix='.log', delete=False)
+        self.filename = temp.name
+        file_handler = logging.FileHandler(temp.name)
+        file_handler.setFormatter(log_formatter)
+        logger.addHandler(file_handler)
 
-            console_handler = logging.StreamHandler(sys.stdout)
-            console_handler.setFormatter(log_formatter)
-            logger.addHandler(console_handler)
-            return logger
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(log_formatter)
+        logger.addHandler(console_handler)
+        self.logger = logger
+        return logger
 
     def flush(self):
         if os.getenv("DKUBE_JOB_CLASS") != "notebook":
             return
-        logger = logging.getLogger()
-        log_paths = [handler.baseFilename for handler in logger.handlers if isinstance(
-            handler, logging.FileHandler)]
-        log_file = next(iter(log_paths))
+
         if mlflow.active_run():
-            mlflow.log_artifact(log_file, "logs")
-            os.remove(log_file)
+            try:
+                mlflow.log_artifact(self.filename, "logs")
+                os.remove(self.filename)
+            except Exception as e:
+                print("Failed to capture logs", e)
         else:
             print("No active run found")
